cf3a5bcbccc19dea635c6aee98c9af9c0755dada
iron_wolf <zeno.dallacqua001@studenti.uniparthenope.it>
2022-02-26 19:26:42 +0100

    aggionamento

--- app.py
+++ app.py
@@ -1,12 +1,46 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, make_response, send_from_directory, request, redirect, json
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("index.html", title="Home", link="/form")
+
+
+@app.route('/test')
+def test():
+    return render_template("index_2.html", title="test")
+
+
+@app.route("/form")
+def form():
+    return render_template("form.html", title="Prenotazione")
+
+
+""""@app.route('/form', methods=['POST'])
+def reserveUp():
+    # read the posted values from the UI
+    _name = request.form['inputName']
+    _surname = request.form['inputName']
+    _cell = request.form['inputCell']
+    _email = request.form['inputEmail']
+    _person = request.form['inputPerson']
+
+    # validate the received values
+    if _name and _surname and _cell and _email and _person:
+        return redirect('/index.html')
+    else:
+        return json.dumps({'html': '<span>Enter the required fields</span>'})
+"""
+
+@app.route('/sw.js')
+def sw():
+    response = make_response(
+        send_from_directory('static', 'sw.js'))
+    response.headers['Content-Type'] = 'application/javascript'
+    return response
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
--- static/js/main.js
+++ static/js/main.js
@@ -1,28 +1,49 @@
 
 window.onload = () => { 'use strict';
-    if ('serviceWorker' in navigator) { navigator.serviceWorker
-        .register('./sw.js').then(function (registration) {
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('/sw.js',{scope:"/"})
+            .then(function (registration) {
                 console.log('ServiceWorker registration successful with scope: ', registration.scope);
             },
             function (err) {
                 console.log('ServiceWorker registration failed: ', err); });
     } }
 
-function test() {
-    console.log("test")
-}
 
-function display_form(table_size,table_num){
+
+
+/*function display_form(table_size,table_num){
     console.log('tableValue: '+table_size+" n:"+table_num)
     let valueTable=document.getElementById("value_table")
     valueTable.innerText="Tavolo N "+table_num+" "+table_size
 
-    document.querySelector('.reserve-form-container').classList.toggle('active');
+    //document.querySelector('.reserve-form-container').classList.toggle('active');
 }
 
 function remove(){
     document.querySelector('.reserve-form-container').classList.remove('active');
 }
+
+$( document ).ready(function() {
+
+    });
+    $(function() {
+        $('prenota').click(function (){
+            $.ajax({
+                url: "/form",
+                data: $('form').serialize(),
+                type: "POST",
+                success: function (response) {
+                    console.log(response);
+                },
+                error: function (error) {
+                    console.log(error);
+                }
+            });
+
+        });
+
+    });*/
 
 /*
 document.querySelector('#form-btn1').onclick = () =>{
